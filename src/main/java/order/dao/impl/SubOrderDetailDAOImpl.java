f1930d46fed88af12ba5e1bf1696d099785be065
Josh <anthony963741@gmail.com>
2023-07-12 17:33:41 +0800

    <新增檔案> 1.CoreDAO hibernate共用VO介面 2.CORE 方法判定回傳共用 <修改檔案> 1.SubOrderDetailDAO(轉hibernate 未完成) 2.SubOrderDetailDAOImpl(轉hibernate 未完成)

---
+++ src/main/java/order/dao/impl/SubOrderDetailDAOImpl.java
@@ -0,0 +1,59 @@
+package order.dao.impl;
+
+import java.sql.Date;
+import java.util.List;
+
+import order.dao.SubOrderDetailDAO;
+import order.entity.SubOrderDetail;
+
+public class SubOrderDetailDAOImpl implements SubOrderDetailDAO{
+
+	@Override
+	public Boolean insert(SubOrderDetail entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SubOrderDetail getByPrimarryKey(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SubOrderDetail> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void upadte_rate(SubOrderDetail SubOrderDetail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void upadte_refundline(Date refundDeadline) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update_refund(SubOrderDetail SubOrderDetail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void item_status(String itemStatus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(SubOrderDetail SubOrderDetail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
