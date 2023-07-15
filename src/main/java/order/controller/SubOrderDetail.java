a60c6e43bb2414eeb5fccc611e4a57ad7b555794
Lulu <lululin0830@gmail.com>
2023-07-15 18:35:45 +0800

    <檔案修改>處理Chrome預檢請求問題問題 1. SubOrderDetail.java 新增doOptions()

--- src/main/java/order/controller/SubOrderDetail.java
+++ src/main/java/order/controller/SubOrderDetail.java
@@ -9,30 +9,39 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
-
-
 /**
  * Servlet implementation class SubOrderDetail
  */
 @WebServlet("/SubOrderDetail")
 public class SubOrderDetail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setHeader("Access-Control-Allow-Origin", "*"); // 允許來自所有網域的請求
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // 允許的 HTTP 方法
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type"); // 允許的請求Header
-		response.setHeader("Access-Control-Allow-Credentials", "true"); // 是否允許帶有憑證的請求
-		response.setContentType("application/json; charset=utf-8");	
-		
-//		Gson gson = new Gson();
+
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		resp.setContentType("application/json; charset=utf-8");
+		resp.setStatus(HttpServletResponse.SC_OK);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setContentType("application/json; charset=utf-8");
+
+		Gson gson = new Gson();
 //		String req = gson.fromJson(request.getReader(),String.class);
-//		
-//		String req = (String)request.getAttribute("searchway");
+
+		String req = (String) request.getAttribute("searchway");
 		System.out.println("有了");
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
--- src/main/webapp/admin/js/Order_List.js
+++ src/main/webapp/admin/js/Order_List.js
@@ -3,7 +3,7 @@ const init = function () {
 
     console.log("進來了");
 
-    fetch('http://localhost:8081/Niigi/SubOrderDetail', {
+    fetch('http://localhost:8080/Niigi/SubOrderDetail', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -12,7 +12,6 @@ const init = function () {
             searchway: document.getElementById("SearchSelect").value
         })
     })
-
     console.log(document.getElementById("SearchSelect").value)
 
 }
