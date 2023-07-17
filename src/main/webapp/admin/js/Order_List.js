ed481c650767ea0a6da7f467cf270f29da01dea0
Josh <anthony963741@gmail.com>
2023-07-17 14:24:44 +0800

    <檔案修改> 後台訂單列表前端修改

--- src/main/webapp/admin/js/Order_List.js
+++ src/main/webapp/admin/js/Order_List.js
@@ -3,16 +3,22 @@ const init = function () {
 
     console.log("進來了");
 
+    const data = JSON.stringify({
+        searchcase: document.getElementById("Searchcase").value,
+        searchway: document.getElementById("SearchSelect").value,
+        StartDate: document.getElementById("StartDate").value,
+        EndDate: document.getElementById("EndDate").value,
+        DateSelect: document.getElementById("DateSelect").value
+    })
+
     fetch('http://localhost:8080/Niigi/SubOrderDetail', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            searchway: document.getElementById("SearchSelect").value
-        })
+        body: data
     })
-    console.log(document.getElementById("SearchSelect").value)
+    console.log(data)
 
 }
 console.log("讀到了")
