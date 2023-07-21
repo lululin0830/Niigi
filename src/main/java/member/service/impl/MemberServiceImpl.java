319f7268a6423b74d3b9072010ce6dc2fead58fe
Jeffrey <jeffrey821537@gmail.com>
2023-07-21 11:23:34 +0800

    <新增檔案>

---
+++ src/main/java/member/service/impl/MemberServiceImpl.java
@@ -0,0 +1,46 @@
+package member.service.impl;
+
+import java.util.List;
+
+import member.entity.Member;
+import member.service.MemberService;
+
+public class MemberServiceImpl implements MemberService{
+
+	@Override
+	public Member register(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member login(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member edit(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean save(Member member) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
