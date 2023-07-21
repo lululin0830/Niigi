ce34171a2f71e33876bfd4677231571978ab5e7f
Lulu <lululin0830@gmail.com>
2023-07-21 11:20:04 +0800

    <新增方法>MemberService 新增註冊、登入方法

--- src/main/java/member/service/MemberService.java
+++ src/main/java/member/service/MemberService.java
@@ -1,7 +1,14 @@
 package member.service;
 
 import core.service.CoreService;
+import member.entity.Member;
 
-public interface MemberService extends CoreService{
+public interface MemberService extends CoreService {
+
+	// 註冊
+	public boolean register(Member newMember);
+
+	// 登入
+	public boolean logIn (Member member);
 
 }
